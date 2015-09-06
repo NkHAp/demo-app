71867efd3e83ff55b2d03dc5af4c062ff94cac1f
himanshu <app2@naklabs.com>
2015-09-06 18:45:36 +0530

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ var redirecturl ; 
+ function gotoserver() {
+                sn = document.getElementById('server').value;
+                redirecturl = 'http://'+sn;
+                //document.location.href=redirecturl;
+        };
+
+		
+var app = {
+	
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+		
+        app.receivedEvent('deviceready');
+		    url = document.getElementById('server').value;
+			alert(url);
+		sessionStorage.openedIAB = 1;		
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+		alert(redirecturl);
+		var ref = cordova.InAppBrowser.open(redirecturl, '_blank', 'location=no,hidden=yes,zoom=no');
+		   var img = document.createElement("img");
+		ref.addEventListener("loadstop", function() {
+			ref.show();
+				//alert("loading stop");
+				 navigator.notification.activityStop();
+		});
+		
+
+		ref.addEventListener("loadstart", function() {
+			//alert("loading start");				 
+			 navigator.notification.activityStart("", "");
+		});
+		
+		ref.addEventListener("loaderror", function() {
+			//alert("loading error");				 
+			 navigator.notification.activityStop();
+		})
+		
+		ref.addEventListener('exit', function(event) {			
+			if (sessionStorage.openedIAB &&  sessionStorage.openedIAB == 1) {
+				sessionStorage.openedIAB = 0;
+				navigator.app.exitApp(); 
+				}
+			});
+		
+    }
+	
+};
